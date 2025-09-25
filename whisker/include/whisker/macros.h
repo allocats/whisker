3c0ad79f8eaa0be4f40496ea51b9fcebfe3f90d4
yukii <lichtestv@gmail.com>
2025-09-25 19:35:24 +0200

    reset

--- whisker/include/whisker/macros.h
+++
@@ -1,9 +0,0 @@
-#ifndef MACROS_H
-#define MACROS_H
-
-#define UNLIKELY(x) __builtin_expect(x, 0)
-#define LIKELY(x) __builtin_expect(x, 1)
-
-#define W_TODO(msg) fprintf(stdout, "%s:%d: \e[1mTODO\e[0m: %s\n", __FILE__, __LINE__, msg)
-
-#endif // !MACROS_H
--- whisker/include/whisker/whisker.h
+++
@@ -1,8 +0,0 @@
-#ifndef WHISKER_H
-#define WHISKER_H
-
-#include "macros.h"
-
-#include "allocators/arena.h"
-
-#endif // !WHISKER_H
